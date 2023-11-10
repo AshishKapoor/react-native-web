7071142d8553ec7de594c2abdc4fb33cd43304e2
Ashish Kapoor <swiftobjc@gmail.com>
2023-11-11 03:46:25 +0530

    update mock api data

--- mock-server/queries/listZellerCustomers.js
+++ mock-server/queries/listZellerCustomers.js
@@ -2,25 +2,25 @@ export const listZellerCustomers = {
     items: [
         {
             id: '1',
-            name: 'TestCustomer1',
+            name: 'Matthew Salmon',
             email: 'test1@test.com',
             role: 'Manager'
         },
         {
             id: '2',
-            name: 'TestCustomer2',
+            name: 'Adman Muller',
             email: 'test2@test.com',
             role: 'Admin'
         },
         {
             id: '3',
-            name: 'TestCustomer3',
+            name: 'Ashish Kapoor',
             email: 'test3@test.com',
             role: 'Manager'
         },
         {
             id: '4',
-            name: 'TestCustomer4',
+            name: 'Perri Smith',
             email: 'test4@test.com',
             role: 'Admin'
         }
